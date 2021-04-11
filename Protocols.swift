cec2fbac9cd61cdebca76cb970d78e748c10706f
Vishnu Prasad M <vpm735@gmail.com>
2021-04-11 20:27:33 +0530

    Update Protocols.swift

--- Protocols.swift
+++ Protocols.swift
@@ -1,8 +1,9 @@
+//A blueprint of Car as a protocol
 protocol Cars{
  var color:String{get set}
  func isAWD() -> Bool
 }
-
+// New class that confirms with protocols with same properties as that of protocol
 class BMW : Cars{
 var color:String=""
 
